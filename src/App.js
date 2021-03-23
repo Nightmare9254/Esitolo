cd859011222667ad107ed6525721ac21f333e9bd
ReworkDelmy <adrian.sarna59@gmail.com>
2021-03-23 20:36:35 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+
+const App = () => {
+  // useEffect(() => {
+  //   fetch('')
+  //     .then((res) => res.json())
+  //     .then((json) => console.log(json));
+  // }, []);
+
+  return <div>JTERAZ MNIE NIE MA</div>;
+};
+
+export default App;
