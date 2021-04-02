34336d1bc52553452b3da355ffaa4522258877c7
ReworkDelmy <adrian.sarna59@gmail.com>
2021-04-02 20:29:57 +0200

    scss file structure, moved to dart scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './components/Main/Main';
+import Pond from './components/Main/Pond';
+
 const App = () => {
   return (
     <div>
       <Router>
         <Switch>
+          {/* <Route exact route="/pond" component={Pond} /> */}
           <Route exact route="/" component={Main} />
         </Switch>
       </Router>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import AppStyle from './AppStyle.scss';
+// import AppStyle from './AppStyle.scss';
+import main from './stylesheets/css/main.css';
 
 ReactDOM.render(
   <React.StrictMode>
