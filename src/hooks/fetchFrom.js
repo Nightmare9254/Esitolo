155d7b0ed404af3f26db7640da735d8c4db76d44
Nightmare9254 <49620354+Nightmare9254@users.noreply.github.com>
2021-05-14 21:52:26 +0200

    Update fetchFrom.js

--- src/hooks/fetchFrom.js
+++ src/hooks/fetchFrom.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:8000';
+const API = 'https://esitolo-backend.herokuapp.com/';
 
 export const fetchFrom = async (endpointURL, options) => {
   const { method, body } = { method: 'POST', body: null, ...options };
