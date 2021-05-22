1a4daccee2f7dbf482a0a5a7167fc6940cb8a1d8
Mikolaj <smgpwwe@wp.pl>
2021-05-22 19:53:56 +0200

    test

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
 
   return (
     <div>
-      <p style={{ fontSize: '28px', color: 'red' }}>LOLOLOLOLO</p>
+      <p style={{ fontSize: '48px', color: 'red' }}>LOLOLOLOLO</p>
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
